aafd0497f1f329620e064b80550564847eb8f4d4
Gustavo7327 <gustavosilva712493@gmail.com>
2023-09-17 13:05:30 -0300

    troca de mensagens funcionando para multiplas mensagens

--- Cliente.java
+++ Cliente.java
@@ -4,31 +4,59 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class Cliente{
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+
+public class Cliente implements ActionListener{
     public static void main(String[] args) {
         
+        // JFrame frame = new JFrame();
+        // JTextField field = new JTextField();
+        
+        // frame.setPreferredSize(new Dimension(300, 300));
+        // frame.setLocationRelativeTo(null);
+        // field.setBounds(100, 100, 100, 100);
+
+        Scanner scanner = new Scanner(System.in);
+
         try {
             Socket socket = new Socket("localhost",3045);
             System.out.println("Cliente conectado");
 
-            Scanner scanner = new Scanner(System.in);
+            
 
             ClienteThread clienteThread = new ClienteThread(socket);
             clienteThread.start();
 
             PrintStream saida = new PrintStream(socket.getOutputStream());
 
-            String teclado = scanner.nextLine();
+            while(true){
+                String teclado = scanner.nextLine();
             
-            saida.println(teclado);
-
-            scanner.close();
+                saida.println(teclado);
+            }
+            
+            
+            
+            //frame.setVisible(true);
+            
         } catch (UnknownHostException e) {
             e.printStackTrace();
+            scanner.close();
 
         } catch (IOException e) {
             e.printStackTrace();
+            scanner.close();
         }
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
         
     }
 }
--- Servidor.java
+++ Servidor.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -19,27 +17,22 @@ public class Servidor {
 
             Socket cliente2 = servidor.accept();
             System.out.println("cliente 2 conectado " + cliente2.getInetAddress().getHostAddress());
-        
-            PrintStream saida = new PrintStream(cliente.getOutputStream());
-            PrintStream saidadois = new PrintStream(cliente2.getOutputStream());
 
-            InputStreamReader inputreader = new InputStreamReader(cliente.getInputStream());
-            BufferedReader bfreader = new BufferedReader(inputreader);
+            PrintStream saida = new PrintStream(cliente2.getOutputStream());
+            PrintStream saida2 = new PrintStream(cliente.getOutputStream());
 
-            InputStreamReader inputreader2 = new InputStreamReader(cliente2.getInputStream());
-            BufferedReader bfreader2 = new BufferedReader(inputreader2);
-
-            String x;
-            String y;
-
-            while((x = bfreader.readLine()) != null && (y = bfreader2.readLine()) != null){
-                saidadois.println("Pessoa 1: " + x);
-                saida.println("Pessoa 2: " + y);
-            }   
+            ServerThread serverThread = new ServerThread(cliente, cliente2, saida);
+            ServerThread serverThread2 = new ServerThread(cliente2, cliente, saida2);
+            
+            while(true){
+                serverThread.start();
+                serverThread2.start();
 
             if(cliente.isConnected() == false || cliente2.isConnected() == false){
                 servidor.close();
             }
+            }
+            
             
         } catch (IOException e) {
             e.printStackTrace();
